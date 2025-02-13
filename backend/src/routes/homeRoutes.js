1bff8917e985c3a05e91be86de7be58d6640558e
Nathan Silva <nathansilva.dev@gmail.com>
2025-02-12 23:15:13 -0300

    backend new massa

---
+++ backend/src/routes/homeRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const router = express.Router();
+const authenticateToken = require("../middlewares/authMiddleware");
+const prisma = new PrismaClient();
+
+// Rota protegida
+router.get("/home", authenticateToken, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.userId },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
+
+    res.json({
+      message: "Bem-vindo à Home, usuário autenticado!",
+      user: {
+        userId: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+});
+
+module.exports = router;
