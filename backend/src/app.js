de6cc01e54f4431f838118ce61cf0e0ae2c35f15
Nathan Silva <nathansilva.dev@gmail.com>
2025-02-14 14:46:07 -0300

    feat- add recuperar senha

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const homeRoutes = require("./routes/homeRoutes");
+const passwordResetRoutes = require("./routes/passwordReset");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use("/api", userRoutes);
 
 app.use("/api", homeRoutes);
+
+app.use("/api", passwordResetRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend rodando corretamente!");
